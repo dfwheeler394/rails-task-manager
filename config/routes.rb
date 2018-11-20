a1843d87d803ffd658e149bdb511e9b9ddc3e4e0
Dean Wheeler <dfwheeler394@gmail.com>
2018-11-20 16:13:40 +0100

    task-manager

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+root 'tasks#index'
+resources :tasks
+patch 'tasks/:id/complete', to: 'tasks#complete', as: :complete
 end
