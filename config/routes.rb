85979c403e40d8deea599977bcd639ebaeb713c0
Dean Wheeler <dfwheeler394@gmail.com>
2019-11-18 11:39:47 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,3 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-
-root 'tasks#index'
-resources :tasks
-patch 'tasks/:id/complete', to: 'tasks#complete', as: :complete
 end
